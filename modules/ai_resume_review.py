cf138efd4db7489f72a036e3de22e13f0a673279
RAJAT RANGRA <rangrajat@gmail.com>
2026-07-09 21:50:20 +0530

    Change Gemini model

--- modules/ai_resume_review.py
+++ modules/ai_resume_review.py
@@ -9,7 +9,7 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+   model="gemini-2.0-flash",
     google_api_key=api_key,
     temperature=0.3
 )
